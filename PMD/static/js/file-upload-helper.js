d0638b13a9cfe8a4260c82ceb090786bee6a25d3
rdoo <->
2015-12-09 01:01:36 +0100

    add file upload form and jquery plugin for triggering bootstrap alerts

---
+++ PMD/static/js/file-upload-helper.js
@@ -0,0 +1,46 @@
+$(function() {
+    var maxFileSizeInBytes = 100000; // 100kB
+    $('#inputFile').change(function () {
+        $('#inputFileResult').hide();
+
+        var file = this.files[0];
+
+        if (file.size > maxFileSizeInBytes) {
+            $.bootstrapGrowl("File is too big", {
+                type: 'danger',
+                align: 'center',
+                width: 'auto'
+            });
+            $('#inputFile').val(null);
+            return;
+        }
+
+        if (file.type != 'text/plain' && file.type != '') {
+            $.bootstrapGrowl("Only text files are allowed", {
+                type: 'danger',
+                align: 'center',
+                width: 'auto'
+            });
+            $('#inputFile').val(null);
+            return;
+        }
+
+        var fileReader = new FileReader();
+        fileReader.readAsText(file);
+
+        fileReader.onload = function (evt) {
+            $('#inputFileResult').html(evt.target.result.replace(/\n/g, '<br>'));
+            $('#inputFileResult').show();
+        }
+
+        fileReader.onerror = function (evt) {
+            $.bootstrapGrowl("Something went wrong", {
+                type: 'danger',
+                align: 'center',
+                width: 'auto'
+            });
+            $('#inputFile').val(null);
+            $('#inputFileResult').hide();
+        }
+    });
+});
