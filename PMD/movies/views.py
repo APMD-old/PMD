b28380291251d6ce62b769d17117010a3a70fc16
Jakub Chmura <chmura.jakub@gmail.com>
2015-12-10 22:01:23 +0100

    Redirect not logged users from home page to login page

---
+++ PMD/movies/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from braces.views import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = 'movies/home.html'
