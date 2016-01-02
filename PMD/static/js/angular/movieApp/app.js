d10707d4b63d9ba3d6ba32c3f1ae058acfef0824
rdoo <rafdomaradzki@gmail.com>
2016-01-02 23:22:23 +0100

    main view

---
+++ PMD/static/js/angular/movieApp/app.js
@@ -0,0 +1,4 @@
+angular.module('movieApp', ['ngResource', 'movieApp.controllers', 'movieApp.services']).config(function($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+});
