b301360314e4926c75ea561227434018a2670ee3
krzjanas <krzjanas@git.com>
2016-01-25 02:19:36 +0100

    Parser return infos about path and file name.

--- PMD/parser/movie_parser.py
+++ PMD/parser/movie_parser.py
@@ -1,13 +1,25 @@
 from guessit import guessit
 
-def guessit_parser(fileName):
-    if not isinstance(fileName, str):
+
+def get_path_name(file_directories):
+    if not isinstance(file_directories, str):
         raise TypeError
 
-    guess = guessit(fileName)
+    head, sep, tail = file_directories.rpartition("/")
+    return head + "/", tail
+
+
+def guessit_parser(file_directories):
+    if not isinstance(file_directories, str):
+        raise TypeError
+
+    path, name = get_path_name(file_directories)
+    guess = guessit(file_directories)
 
     dictionary = {}
-    dictionary["type"] = guess.get("type","unknown")
+    dictionary["path"] = path
+    dictionary["file_name"] = name
+    dictionary["type"] = guess.get("type", "unknown")
     dictionary["title"] = guess.get("title")
     dictionary["year"] = guess.get("year")
     dictionary["series"] = guess.get("series")
@@ -16,12 +28,13 @@ def guessit_parser(fileName):
 
     return dictionary
 
-def movies_parser(directories):
-    list = []
-    for dir in directories:
-        list.append(guessit_parser(dir))
 
-    return list
+def movies_parser(directories):
+    l = []
+    for d in directories:
+        l.append(guessit_parser(d))
+
+    return l
 
 
 
--- PMD/parser/tests/test_movies_parser.py
+++ PMD/parser/tests/test_movies_parser.py
@@ -1,7 +1,18 @@
 from unittest import TestCase
-from PMD.parser.movie_parser import guessit_parser, movies_parser
+from PMD.parser.movie_parser import *
 
 class TestMovies_parser(TestCase):
+    def test_get_path_name(self):
+        self.assertRaises(TypeError,get_path_name,1)
+
+        file1 = './Epoka Lodowcowa/Epoka Lodowcowa.avi'
+        file2 = 'Kły/1x2.mp4'
+        file3 = '1x2.mp4'
+
+        self.assertEqual(get_path_name(file1),('./Epoka Lodowcowa/','Epoka Lodowcowa.avi'))
+        self.assertEqual(get_path_name(file2),('Kły/','1x2.mp4'))
+        self.assertEqual(get_path_name(file3),('/','1x2.mp4'))
+
     def test_guessit_parser(self):
         self.assertRaises(TypeError,guessit_parser,1)
 
@@ -9,24 +20,28 @@ class TestMovies_parser(TestCase):
         file2 = './Kły/Kły.2014.mp4'
         file3 = 'Kły/1x2.mp4'
 
-        dict1 = {'episode': None, 'type': 'movie', 'title': 'Epoka Lodowcowa', 'series': None, 'season': None, 'year': None}
-        dict2 = {'title': 'Kły', 'season': None, 'episode': None, 'year': 2014, 'type': 'movie', 'series': None}
-        dict3 = {'title': 'Kły', 'season': 1, 'episode': 2, 'year': None, 'type': 'episode', 'series': None}
+        dict1 = {'path': './Epoka Lodowcowa/','episode': None, 'type': 'movie', 'title': 'Epoka Lodowcowa',
+                 'file_name': 'Epoka Lodowcowa.avi', 'series': None, 'season': None, 'year': None}
+        dict2 = {'file_name': 'Kły.2014.mp4','path': './Kły/','title': 'Kły', 'season': None, 'episode': None,
+                 'year': 2014, 'type': 'movie', 'series': None}
+        dict3 = {'path': 'Kły/','file_name': '1x2.mp4','title': 'Kły', 'season': 1, 'episode': 2,
+                 'year': None, 'type': 'episode', 'series': None}
 
         self.assertEqual(guessit_parser(file1),dict1)
         self.assertEqual(guessit_parser(file2),dict2)
         self.assertEqual(guessit_parser(file3),dict3)
 
-
     def test_movies_parser(self):
-        list = []
-        list.append('./Epoka Lodowcowa/Epoka Lodowcowa.avi')
-        list.append('./Kły/Kły.2014.mp4')
+        l = []
+        l.append('./Epoka Lodowcowa/Epoka Lodowcowa.avi')
+        l.append('./Kły/Kły.2014.mp4')
 
-        dict1 = {'episode': None, 'type': 'movie', 'title': 'Epoka Lodowcowa', 'series': None, 'season': None, 'year': None}
-        dict2 = {'title': 'Kły', 'season': None, 'episode': None, 'year': 2014, 'type': 'movie', 'series': None}
+        dict1 = {'path': './Epoka Lodowcowa/','episode': None, 'type': 'movie', 'title': 'Epoka Lodowcowa',
+                 'file_name': 'Epoka Lodowcowa.avi', 'series': None, 'season': None, 'year': None}
+        dict2 = {'file_name': 'Kły.2014.mp4','path': './Kły/','title': 'Kły', 'season': None, 'episode': None,
+                 'year': 2014, 'type': 'movie', 'series': None}
 
-        par = movies_parser(list)
+        par = movies_parser(l)
 
         self.assertEqual(par[0],dict1)
         self.assertEqual(par[1],dict2)
